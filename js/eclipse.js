6ba67b1cb3663dcfe3323af1bd94e31b5a32c9ec
Aaron <betterwithketchup@gmail.com>
2018-06-06 20:34:37 -0400

    Eclipse 2

--- js/eclipse.js
+++ js/eclipse.js
@@ -2,12 +2,20 @@
     $('select').material_select();
     var dis = $("#disabled").val();
     document.getElementById("disabled").value = $("#cog").val()+$("#int").val()+$("#ref").val()+$("#sav").val()+$("#som").val()+$("#wil").val();
-    console.log("Ping")
+    console.log("Ping");
+
  });
 
- $('#cog').on('update',function(event){
+ /*$('#cog').on('update',function(event){
 	event.preventDefault();
     document.getElementById("disabled").value = $("#cog").val()+$("#int").val()+$("#ref").val()+$("#sav").val()+$("#som").val()+$("#wil").val();
 
 	
+})*/
+ $('#apt').on('update',function(event){
+	event.preventDefault();
+	var apt = $("#apt").val();
+	if (apt==1) {
+		document.getElementById("cog").value = 10;
+	}
 })
